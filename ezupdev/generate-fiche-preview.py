99f6ebd3c4bd75370768cefb48ed2762e91c0756
Dash Eclipse <dashezup@disroot.org>
2021-03-30 06:02:27 +0000

    ezupdev: update generate-fiche-preview.py

--- ezupdev/generate-fiche-preview.py
+++ ezupdev/generate-fiche-preview.py
@@ -10,7 +10,7 @@ import subprocess
 from PIL import Image
 from pygments import highlight
 from pygments.lexers import guess_lexer
-from pygments.formatters import HtmlFormatter, JpgImageFormatter
+from pygments.formatters import HtmlFormatter, ImageFormatter
 
 fiche_log = '/var/log/fiche/log'
 fiche_dir = '/var/data/fiche'
@@ -30,7 +30,7 @@ DOC_HTML = '''\
 <meta property="og:title" content="Paste | ezup.dev/p" />
 <meta property="og:description" \
 content="Upload your pastes through netcat/bash/zsh - https://ezup.dev/p" />
-<meta property="og:image" content="preview.jpg" />
+<meta property="og:image" content="preview.png" />
 <meta property="og:image:width" content="{width}">
 <meta property="og:image:height" content="{height}">
 <link rel="stylesheet" href="../styles.css" type="text/css" />
@@ -41,22 +41,30 @@ content="Upload your pastes through netcat/bash/zsh - https://ezup.dev/p" />
 '''
 
 
-async def generate_html_and_jpg(fiche_id):
+async def generate_html_and_png(fiche_id):
     fiche_paste = os.path.join(fiche_dir, fiche_id)
     txt = os.path.join(fiche_paste, 'index.txt')
     html = os.path.join(fiche_paste, 'index.html')
-    jpg = os.path.join(fiche_paste, 'preview.jpg')
-    with open(txt) as f_txt, open(jpg, 'wb') as f_jpg:
+    png = os.path.join(fiche_paste, 'preview.png')
+    with open(txt) as f_txt, open(png, 'wb') as f_png:
         code = f_txt.read()
         preview_code = "\n".join(code.split('\n')[:79])
         lexer = guess_lexer(code)
-        f_jpg.write(highlight(preview_code, lexer, JpgImageFormatter()))
-    with open(html, 'w') as f_html, Image.open(jpg) as im:
+        image_formatter = ImageFormatter(
+            font_name="Fira Mono",
+            line_number_bg="#073642",
+            line_number_fg="#586e75",
+            style="solarized-dark"
+        )
+        f_png.write(highlight(preview_code, lexer, image_formatter))
+    with open(html, 'w') as f_html, Image.open(png) as im:
         width, height = im.size
-        formatter = HtmlFormatter(linenos=True, full=False)
+        formatter = HtmlFormatter(linenos=True, full=False, style="stata-dark")
         code_html = highlight(code, lexer, formatter)
         full_html = DOC_HTML.format(
-            code_html=code_html, width=width, height=height
+            code_html=code_html,
+            width=width,
+            height=height
         )
         f_html.write(full_html)
 
@@ -70,7 +78,7 @@ async def main():
                 continue
             fiche_id = line.split()[0]
             print(fiche_id)
-            await generate_html_and_jpg(fiche_id)
+            await generate_html_and_png(fiche_id)
     except KeyboardInterrupt:
         pass
 
